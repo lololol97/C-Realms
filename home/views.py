33987401fa64baf28aced22db2d38088deb79ca2
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-08-13 16:46:36 -0400

    doin work, pls change your admin password ragav

--- home/views.py
+++ home/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 def index(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return render(request, "home.html")
+        else:
+            return render(request, 'login.html')
+
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
