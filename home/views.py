2ddefce8216b7029f597ccd93ac9fc2c184c9fe1
lololol97 <sriragav.naresh@gmail.com>
2021-08-13 14:59:01 -0400

    bibi

--- home/views.py
+++ home/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username = "username", password = "password")
+    else:
+        return render(request, 'login.html')
+
+    if user is not None:
+        auth.login(request, user)
+        return redirect("/")
+    else:
+        messages.error(request, 'invalid boboboy')
+        return redirect("login")
