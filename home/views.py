56272938af7676b74fa453c28990f278418f356e
lololol97 <sriragav.naresh@gmail.com>
2021-08-14 11:37:38 -0400

    fixed working login page

--- home/views.py
+++ home/views.py
@@ -2,23 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 
 def index(request):
-
-    return render(request, "home.html")
-
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return render(request, "home.html")
-        else:
-            return render(request, 'login.html')
-
-    else:
-        return render(request, 'login.html')
+    return render(request, "login.html")
 
 
 def logout(request):
