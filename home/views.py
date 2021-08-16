0bf0ccb9c6fb10a377d10098d2a69d74aa65f778
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2021-08-16 11:20:45 -0400

    lololol

--- home/views.py
+++ home/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 
 def index(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html')
+
+    else:
+        return render(request, 'login.html')
 
 
 def logout(request):
     auth.logout(request)
     return redirect('/')
-
